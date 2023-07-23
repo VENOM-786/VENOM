52761eea4e8994dfbddbda982df767692935bd69
MaNi RaJpOoT <113978329+VENOM-786@users.noreply.github.com>
2023-07-23 15:35:47 +0500

    Update VENOM.py

--- VENOM.py
+++ VENOM.py
@@ -1,8 +1,8 @@
 import os,platform
 os.system('git pull')
- 
+ exit(' Wait Tool On updating ')
 VENOM=platform.architecture()[0]
 if VENOM=="32bit":
-    __import__("BSDK32")
+    #__import__("BSDK32")
 elif VENOM=="64bit":
-     __import__("BSDK")
+   #  __import__("BSDK")
