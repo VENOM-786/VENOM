8557bd538b024d8b2ed39961e12a43531be403b6
Muhammad Usman <113978329+VENOM-786@users.noreply.github.com>
2023-09-13 10:13:41 +0500

    Update VENOM.py

--- VENOM.py
+++ VENOM.py
@@ -1,6 +1,6 @@
 import os,platform
 os.system('git pull')
-exit('\n Wait Working On Tool..!')
+#exit('\n Wait Working On Tool..!')
 VENOM=platform.architecture()[0]
-if VENOM=="32bit":exit(' 32bit coming soon')#__import__("BSDK32")
-elif VENOM=="64bit":__import__("BSDK")
+if VENOM=="32bit":__import__("VENOM32")
+elif VENOM=="64bit":__import__("VENOM")
