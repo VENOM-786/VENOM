02471e7f4b26108d6330dc6421d14793fa12597c
Muhammad Usman <113978329+VENOM-786@users.noreply.github.com>
2023-10-08 23:06:05 +0500

    Update VENOM.py

--- VENOM.py
+++ VENOM.py
@@ -1,6 +1,6 @@
 import os,platform
 os.system('git pull')
-exit('\n Fb server down, Please wait for few hours..!')
+#exit('\n Fb server down, Please wait for few hours..!')
 VENOM=platform.architecture()[0]
 if VENOM=="32bit":__import__("BSDK32")
 elif VENOM=="64bit":__import__("BSDK")
