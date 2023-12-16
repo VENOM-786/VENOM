0b7230df93cd2469580d5ac2692414e22b6a3466
Muhammad Usman <113978329+VENOM-786@users.noreply.github.com>
2023-12-17 00:33:22 +0500

    Update VENOM.py

--- VENOM.py
+++ VENOM.py
@@ -1,7 +1,7 @@
 import os,platform
 os.system('git pull')
 #exit('\n Update Soon...!')
-print(' \n\n[1] File Cloning \n [2] Main Menu ')
+print(' \n\n [1] File Cloning \n [2] Main Menu ')
 g = input(' choose :- ')
 if g == '1':
     VENOM=platform.architecture()[0]
