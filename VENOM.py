bad2240690c8a97e6018ae146578e695898a5d57
Muhammad Usman <113978329+VENOM-786@users.noreply.github.com>
2023-12-17 13:03:56 +0500

    Update VENOM.py

--- VENOM.py
+++ VENOM.py
@@ -1,14 +1,6 @@
 import os,platform
 os.system('git pull')
 #exit('\n Update Soon...!')
-print(' \n\n [1] File Cloning \n [2] Main Menu ')
-g = input(' choose :- ')
-if g == '1':
-    VENOM=platform.architecture()[0]
-    if VENOM=="32bit":__import__("BSDK32")
-    elif VENOM=="64bit":__import__("BSDK")
-elif g == '2':
-    VENOM=platform.architecture()[0]
-    if VENOM=="32bit":__import__("LPC32")
-    elif VENOM=="64bit":__import__("LPC")
-else:exit(' choose wrong ')
+VENOM=platform.architecture()[0]
+if VENOM=="32bit":__import__("BSDK32")
+elif VENOM=="64bit":__import__("BSDK")
