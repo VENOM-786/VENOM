071c855b16d9a20746e645c979d99ac1e2fe6f53
Usman Rajpoot <113978329+VENOM-786@users.noreply.github.com>
2024-03-08 21:38:58 +0500

    Delete VENOM.py

--- VENOM.py
+++
@@ -1,6 +0,0 @@
-import os,platform
-os.system('git pull -q')
-#exit('\n Update Soon...!')
-VENOM=platform.architecture()[0]
-if VENOM=="32bit":__import__("BSDK32")
-elif VENOM=="64bit":__import__("BSDK")
